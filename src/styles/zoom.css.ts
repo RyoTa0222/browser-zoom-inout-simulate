c7a23a43364741eca942a47b1c5becfdc156329e
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-04-14 23:44:45 +0900

    feat: change css

--- src/styles/zoom.css.ts
+++ src/styles/zoom.css.ts
@@ -3,30 +3,21 @@ import { ZoomType } from "../types/zoom";
 
 export const ZoomBox = styled.div<{ "scale-correction": number }>`
   background: #252528;
-  border: 0.12vw solid #040405;
+  border: 2px solid #040405;
   box-sizing: border-box;
-  border-radius: 0.4vw;
-  padding: 0.4vw 1vw;
-  box-shadow: 0px 0.4vw 1vw 0.4vw rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  padding: 8px 24px;
+  box-shadow: 0px 4px 16px 8px rgb(0 0 0 / 20%);
   display: flex;
   align-items: center;
-  width: 32vw;
-  max-width: 300px;
+  width: 400px;
   transform-origin: right top;
   transform: scale(${(props) => 1 / props["scale-correction"]});
   margin-top: ${(props) => 2 / props["scale-correction"]}vw;
   margin-right: ${(props) => 2 / props["scale-correction"]}vw;
-  @media only screen and (max-width: 820px) {
-    width: 50vw;
-    max-width: 400px;
-    padding: 1vw 1.6vw;
-    border-radius: 1vw;
-  }
   @media only screen and (max-width: 390px) {
-    width: 60vw;
-    max-width: 500px;
-    padding: 1.2vw 2vw;
-    border-radius: 1.2vw;
+    width: 90vw;
+    padding: 4px 12px;
   }
 `;
 
@@ -40,13 +31,10 @@ export const DisplaySizeText = styled.span`
   font-family: inherit;
   font-style: normal;
   font-weight: bold;
-  font-size: 0.9vw;
+  font-size: 16px;
   color: #e4e7ea;
-  @media only screen and (max-width: 820px) {
-    font-size: 2vw;
-  }
   @media only screen and (max-width: 390px) {
-    font-size: 3vw;
+    font-size: 12px;
   }
 `;
 
@@ -60,9 +48,9 @@ export const ZoomSwitchButton = styled.button<{
   background: #252528;
   border: none;
   position: relative;
-  margin: 0 0.4vw;
-  width: 2vw;
-  height: 2vw;
+  margin: 0 4px;
+  width: 32px;
+  height: 32px;
   border-radius: 50%;
   transition: background 0.2s;
   &:hover {
@@ -75,8 +63,8 @@ export const ZoomSwitchButton = styled.button<{
     position: absolute;
     left: 50%;
     top: 50%;
-    width: 1vw;
-    height: 0.2vw;
+    width: 16px;
+    height: 2px;
     background-color: #8f969c;
     transform: translate(-50%, -50%);
   }
@@ -87,56 +75,37 @@ export const ZoomSwitchButton = styled.button<{
         ? "translate(-50%, -50%) rotate(90deg)"
         : "translate(-50%, -50%)"};
   }
-  @media only screen and (max-width: 820px) {
-    margin: 0 0.6vw;
-    width: 3.2vw;
-    height: 3.2vw;
-    &::before,
-    &::after {
-      width: 1.6vw;
-      height: 0.3vw;
-    }
-  }
   @media only screen and (max-width: 390px) {
-    margin: 0 1.2vw;
-    width: 4vw;
-    height: 4vw;
+    width: 24px;
+    height: 24px;
     &::before,
     &::after {
-      width: 3vw;
-      height: 0.6vw;
+      width: 12px;
+      height: 2px;
     }
   }
 `;
 
 export const ZoomResetButton = styled.button`
   background: #252528;
-  border: 0.2vw solid #35383b;
+  border: 3px solid #35383b;
   box-sizing: border-box;
-  border-radius: 0.4vw;
+  border-radius: 4px;
   font-family: inherit;
   font-style: normal;
   font-weight: 700;
-  font-size: 1vw;
+  font-size: 16px;
   color: #9cbaee;
-  padding: 0.8vw 1.2vw;
-  margin-left: 1vw;
+  padding: 8px 16px;
+  margin-left: 24px;
   transition: background 0.2s;
   &:hover {
     cursor: pointer;
     background: #36363a;
   }
-  @media only screen and (max-width: 820px) {
-    font-size: 1.4vw;
-    border-width: 0.3vw;
-    margin-left: 2vw;
-    padding: 0.8vw 2vw;
-  }
   @media only screen and (max-width: 390px) {
-    font-size: 3vw;
-    border-width: 0.6vw;
-    margin-left: 3vw;
-    padding: 0.8vw 2vw;
+    font-size: 12px;
+    margin-left: 12px;
   }
 `;
 
