7d1dffed97cfb3505ca50e1e2c31770b0087b729
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-04-16 02:50:14 +0900

    refactor: Constant outings

---
+++ src/consts/config.ts
@@ -0,0 +1,10 @@
+// 拡縮の間隔
+export const SCALE_INTERVAL = 10;
+// 最小のズーム率
+export const MINIMUM_ZOOM_RATIO = 60;
+// 最大のズーム率
+export const MAXIMUM_ZOOM_RATIO = 120;
+// 変更できないアラートの文言
+export const ALERT_MESSAGE = `表示サイズは${MINIMUM_ZOOM_RATIO}%から${MAXIMUM_ZOOM_RATIO}%まで変更可能です`;
+// 表示サイズの初期値
+export const INITIAL_DISPLAY_SIZE = 100;
